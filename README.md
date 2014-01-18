830c8c90c56629d8f6ba25c16ffc6334dc23d7a2
xingzhong <xu.xeno@gmail.com>
2014-01-18 10:39:12 -0500

    init

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 PandasTA
 ========
 
-This is a small script to do technique analysis on stock data from Yahoo Finance
+This is a small script to do technique analysis on stock data from Yahoo Finance.
+
+It use python-pandas to read historical stock data, 
+and then use TA-lib to calculate several essential technique analysis indicators.
+
+The input is simple list of ticker names.
+The output is pandas' panel data structure which contains all relevant information.
+
+# requirements
+1. pandas
+2. TA-Lib
+
+# Example 
+	tickers = ['AAPL', "AMZN", "SOHU", "WMT"]
+	panel = load(tickers)
+	print panel
+	print panel.minor_xs('AAPL')[['Close', 'BB_per', "MACD_Hist"]].tail()
+
+	
+	<class 'pandas.core.panel.Panel'>
+	Dimensions: 22 (items) x 126 (major_axis) x 4 (minor_axis)
+	Items axis: Open to DCPhase
+	Major_axis axis: 2013-07-22 00:00:00 to 2014-01-17 00:00:00
+	Minor_axis axis: AAPL to WMT
+
+				Close    BB_per  MACD_Hist
+	Date                                   
+	2014-01-13  535.73  0.362429  -3.609874
+	2014-01-14  546.39  0.864649  -2.604802
+	2014-01-15  557.36  0.931808  -1.143649
+	2014-01-16  554.25  0.729639  -0.369175
+	2014-01-17  540.67  0.308106  -0.733028
---
+++ pandasTA.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from pandas.io.data import DataReader
+from datetime import datetime, timedelta
+import talib
+
+def load(tickers, start=datetime.now()-timedelta(days=180), 
+			end=datetime.now()):
+	"""load data from yahoo finance and do technique analysis
+
+	Args:
+		tickers (list) : list of valid stock tickers
+		start (datetime) : historical data start date
+		end (datetime) : historical data end date
+
+	Returns:
+		panel (pandas.panel) : a 3-dimensional data structure 
+			Item axis : including Open, Close, High, Volume and 
+			MACD, etc. TA indicators. 
+			Major axis: historical datetime index
+			minor axis: tickers
+	"""
+	panel = DataReader(tickers, "yahoo", start, end)
+	panelCopy = panel.transpose(2, 1, 0, copy=True)
+	smaDf = pd.DataFrame(index=panel.major_axis)
+	BBUDf = pd.DataFrame(index=panel.major_axis)
+	BBDDf = pd.DataFrame(index=panel.major_axis)
+	BBMDf = pd.DataFrame(index=panel.major_axis)
+	CCIDf = pd.DataFrame(index=panel.major_axis)
+	MACDDf = pd.DataFrame(index=panel.major_axis)
+	MACDSDf = pd.DataFrame(index=panel.major_axis)
+	MACDHDf = pd.DataFrame(index=panel.major_axis)
+	MFIDf = pd.DataFrame(index=panel.major_axis)
+	MOMDf = pd.DataFrame(index=panel.major_axis)
+	RSIDf = pd.DataFrame(index=panel.major_axis)
+	StochSlowDf = pd.DataFrame(index=panel.major_axis)
+	RSIDf = pd.DataFrame(index=panel.major_axis)
+	WillDf = pd.DataFrame(index=panel.major_axis)
+	HilbertPeriodDf = pd.DataFrame(index=panel.major_axis)
+	HilbertPhaseDf = pd.DataFrame(index=panel.major_axis)
+
+	for (t, df) in panelCopy.iteritems():
+	    smaDf[t] = talib.SMA(df.Close)
+	    BBUDf[t], BBMDf[t], BBDDf[t] = talib.BBANDS(df.Close)
+	    CCIDf[t] = talib.CCI(df.High, df.Low, df.Close)
+	    MACDDf[t], MACDSDf[t], MACDHDf[t] = talib.MACD(df.Close)
+	    MFIDf[t] = talib.MFI(df.High, df.Low, df.Close, df.Volume)
+	    MOMDf[t] = talib.MOM(df.Close)
+	    RSIDf[t] = talib.RSI(df.Close)
+	    WillDf[t] = talib.WILLR(df.High, df.Low, df.Close)
+	    HilbertPeriodDf[t] = talib.HT_DCPERIOD(df.Close)
+	    HilbertPhaseDf[t] = talib.HT_DCPHASE(df.Close)
+	    
+	    
+	panel['SMA'] = smaDf
+	panel['BB_upper'] = BBUDf
+	panel['BB_center'] = BBMDf
+	panel['BB_lower'] = BBDDf
+	panel['BB_per'] = (panel.Close - panel.BB_lower) / (
+						panel.BB_upper - panel.BB_lower)
+	panel['BB_width'] = (panel.BB_upper - panel.BB_lower) / panel.BB_center
+	panel['CCI'] = CCIDf
+	panel['MACD'] = MACDDf
+	panel['MACD_Signal'] = MACDSDf
+	panel['MACD_Hist']= MACDHDf
+	panel['MFI'] = MFIDf
+	panel['MOM'] = MOMDf
+	panel['RSI'] = RSIDf
+	panel['Will'] = WillDf
+	panel['DCPeriod'] = HilbertPeriodDf
+	panel['DCPhase'] = HilbertPhaseDf
+	return panel
+
+if __name__ == '__main__':
+	tickers = ['AAPL', "AMZN", "SOHU", "WMT"]
+	panel = load(tickers)
+	print panel
+	print panel.minor_xs('AAPL')[['Close', 'BB_per', "MACD_Hist"]].tail()
